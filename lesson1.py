0b773ecab47a6e24a8a30c51f56cef50617b8d35
Vladimir Ustyanivsky <bender1102@gmail.com>
2023-06-20 21:16:01 +0300

    Initial commit

---
+++ lesson1.py
@@ -0,0 +1,11 @@
+# print('Enter two numbers and find out the remainder')
+#
+# first_operand = int(input('Input first number: '))
+# second_operand = int(input('Input second number: '))
+# result = first_operand % second_operand
+#
+# print(f'Remainder is {result}')
+#
+#
+# print(bool(result))
+
